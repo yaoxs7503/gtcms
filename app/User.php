625421264fd5ebb980ad065874092f1b311b422b
philip_windows10 <348180212@qq.com>
2020-02-23 23:56:12 +0800

    增加Glup打包工具

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Role;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -23,4 +24,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+   public function role()
+   {
+      return $this->belongsTo(Role::class); 
+   }     
 }
